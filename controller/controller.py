75ec771c2a049ac7928badbe474e8359ba8811a2
Alan Boris <39385685+alanb128@users.noreply.github.com>
2023-07-13 00:11:02 -0400

    Update controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -18,15 +18,19 @@ from adafruit_seesaw import seesaw, rotaryio
 
 # Adafruit I2C QT Rotary Encoder
 # Using the INT output on Pi GPIO 17
-seesaw = seesaw.Seesaw(board.I2C(), addr=0x36)
-seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
-print("Found seesaw supported product {}".format(seesaw_product))
-if seesaw_product != 4991:
-    print("Wrong firmware loaded for QT encoder?  Expected 4991")
-# Set up the rotary click button, and add to interrupt
-seesaw.pin_mode(24, seesaw.INPUT_PULLUP)  # Pin on the QT
-seesaw.set_GPIO_interrupts(1 << 24, True)
-seesaw.enable_encoder_interrupt()
+try:
+    seesaw = seesaw.Seesaw(board.I2C(), addr=0x36)
+except:
+    print("Error initializing the rotary encoder board.")
+else:
+    seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
+    print("Found seesaw supported product {}".format(seesaw_product))
+    if seesaw_product != 4991:
+        print("Wrong firmware loaded for QT encoder?  Expected 4991")
+    # Set up the rotary click button, and add to interrupt
+    seesaw.pin_mode(24, seesaw.INPUT_PULLUP)  # Pin on the QT
+    seesaw.set_GPIO_interrupts(1 << 24, True)
+    seesaw.enable_encoder_interrupt()
 
 # TFT configuration for CS and DC pins:
 cs_pin = digitalio.DigitalInOut(board.CE0) #pin24
@@ -78,6 +82,10 @@ idle_image = Image.new("RGB", (width, height))
 # image for about screen
 img_about = Image.new("RGB", (width, height))
 
+# tiny images for fwd/rev wheel icon
+img_right = Image.new('RGB', (20, 20), (0, 0, 0))
+img_left = Image.new('RGB', (20, 20), (0, 0, 0))
+
 # Drawing object for main display image
 draw = ImageDraw.Draw(image)
 
@@ -88,20 +96,32 @@ noise_pointer = 0  # index of currently displayed noise image when rotating whee
 rotary_pos = 0  # keeps track of wheel position
 now_playing = ""  # name of currently playing file or emply string
 display_mode = 0  # int display mode (0-2)
-current_volume = 10  # current volume (0-20)
+current_volume = 10  # default current volume if none saved (0-20)
+new_volume = 10 # variable to see if volume has changed
 loop_count = 0  # loop counter for checking the current sound status
-IMAGE_PATH = "/data/my_data/noise/"  # string path to images
+IMAGE_PATH = os.getenv('MEDIA_PATH', "/data/my_data/noise/")  # string path to noise and images
+ASSET_PATH = os.getenv('ASSET_PATH', "/data/my_data/assets/") # string path to non-noise assets
 TEXT_FONT = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
 ICON_FONT = ImageFont.truetype("/usr/src/app/materialdesignicons-webfont.ttf", 20)
 DEBOUNCE = 600  # switch debounce time in ms
+rot_first = datetime.datetime.now()
+rot_last = datetime.datetime.now()
+
+P1_GPIO = 6
+P2_GPIO = 13
+P3_GPIO = 12 # was 19
+P4_GPIO = 20
+DISP_GPIO = 22 # was 21
+STOP_GPIO = 5
 
 def lib_setup():
     #
     # Generate list of all sound files available,
     # create list of all images with titles burned in,
     # create volume image list
+    # create wheel icon images
     #
-    global wav_list, img_list, img_info
+    global wav_list, img_list, img_info, img_left, img_right
     # Get all sound files in folder
     dir_list = os.listdir(IMAGE_PATH)
     font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
@@ -116,58 +136,74 @@ def lib_setup():
             try:
                 i = Image.open(IMAGE_PATH + noise_name + ".jpg")
             except:
-                i = Image.open(IMAGE_PATH + "default.jpg")
+                i = Image.open(ASSET_PATH + "default.jpg")
             save_image.paste(i, (0,0))
             save_image.paste(img_info, (0, 108))
             text = noise_name.replace("_", " ")
             save_draw.text((20, 110), text, font=font, fill=(255, 255, 255),)
             img_list.append(save_image)
     # generate a list of 20 vol images:
-    for v in range(0, 19):
+    for v in range(0, 21):
         save_image = Image.new("RGB", (width, height))
         save_draw = ImageDraw.Draw(save_image)
         save_draw.rectangle((0, 0, width, height), fill=(0, 0, 0))  # draw black box
-        i = Image.open(IMAGE_PATH + "vol_" + "{:0>3}".format(v * 5) + ".jpg")
+        i = Image.open(ASSET_PATH + "vol_" + "{:0>3}".format(v * 5) + ".jpg")
         save_image.paste(i, (0,50))
         text = "volume: " + str(v * 5) + " %"
         save_draw.text((20, 110), text, font=font, fill=(255, 255, 255),)
         save_draw.text((1, 110), "\U000F057F", font=ICON_FONT, fill=(255, 255, 255),)
         vol_list.append(save_image)
 
+    # wheel icon images
+    wheel_draw = ImageDraw.Draw(img_left)
+    wheel_draw.text((1, 1), "\U000F04D5", font=ICON_FONT, fill=(255, 255, 255),)  # draw the left sel icon
+    wheel_draw = ImageDraw.Draw(img_right)
+    wheel_draw.text((1, 1), "\U000F04D7", font=ICON_FONT, fill=(255, 255, 255),)  # draw the right sel icon
+
 def create_idle_image():
     #
     # Create the idle image!
     #
     global idle_image
+    idle_image_raw = Image.new("RGB", (width, height))
     try:
-        idle_image_raw = Image.open(IMAGE_PATH + "idle.jpg")
+        idle_image_raw = Image.open(ASSET_PATH + "idle.jpg")
     except:
         # if no idle image found, draw a rectangle instead
-        idle_image_raw = idle_image_raw.rectangle((0, 0, width, height), fill=(0, 100, 200))
+        idle_image_draw = ImageDraw.Draw(idle_image_raw)
+        idle_image_draw.rectangle((0, 0, width, height), fill=(0, 100, 200))
     idle_image.paste(idle_image_raw, (0,0))  # use idle image
     idle_draw = ImageDraw.Draw(idle_image)
     idle_image.paste(img_info, (0, 108))
     idle_draw.text((1, 110), "\U000F04DB", font=ICON_FONT, fill=(255, 255, 255),)  # draw the stop icon
-    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
-    idle_draw.text((20, 110), "(idle)", font=font, fill=(255, 255, 255),)
+    #font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+    idle_draw.text((20, 110), "(idle)", font=TEXT_FONT, fill=(255, 255, 255),)
 
 # Setup initial state
 def init_controller():
     print("init display")
     global disp, image, draw, now_playing, idle_image
 
-    # Draw a blue box for init display
-    draw.rectangle((0, 0, width, height), fill=(0, 0, 72))
+    try:
+        image = Image.open(ASSET_PATH + "splash.jpg")
+    except:
+        print("Error accessing splash.jpg - using default splash screen instead.")
+        # Draw a blue box for init display
+        draw.rectangle((0, 0, width, height), fill=(0, 0, 72))
 
-    # Load a TTF Font
-    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+        # Load a TTF Font
+        font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
 
-    # Draw splash screen
-    text = "Welcome!"
-    draw.text((3, 10), text, font=font, fill=(255, 255, 0),)
-    disp.image(image)
+        # Draw splash screen
+        text = "Welcome!"
+        draw.text((3, 10), text, font=font, fill=(255, 255, 0),)
+    else:
+        disp.image(image)
+    time.sleep(1)
     lib_setup()
+    time.sleep(1)
     create_idle_image()
+    time.sleep(1)
     display_info()
 
     # get current status
@@ -262,7 +298,7 @@ def button_preset(channel):
     # play a preset
     #
     # get preset number (1-4) from channel
-    preset_channel = [6, 13, 19, 26]
+    preset_channel = [P1_GPIO, P2_GPIO, P3_GPIO, P4_GPIO]
     presety = preset_channel.index(channel)
     # get preset name value from redis
     r = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
@@ -293,6 +329,9 @@ def play_file(noise_name, extra_icon=""):
     # Play the specified file, update the display
     #
     global now_playing, noise_pointer, display_mode
+    if noise_name == '':
+        print("Warning! Noise with no name requested.")
+        return
     display_mode = 0
     now_playing = noise_name
     noise_pointer = wav_list.index(now_playing)
@@ -304,29 +343,32 @@ def display_now(d_img, extra_icon=""):
     # sends specified image to lcd display
     # if "" then display idle image
     #
-    global disp, image, draw, display_timer
-
-    image.paste(d_img, (0,0))  # use now playing image
+    
+    d_img_draw = ImageDraw.Draw(d_img)
+    #image.paste(d_img, (0,0))  # use now playing image
     #print("Now playing: {0}, wav_list.index(now_playing): {1}".format(now_playing, wav_list.index(now_playing)))
-    draw.text((1, 110), "\U000F040A", font=ICON_FONT, fill=(255, 255, 255),)  # draw the play icon
+    d_img_draw.text((1, 110), "\U000F040A", font=ICON_FONT, fill=(255, 255, 255),)  # draw the play icon
     if len(extra_icon) > 0:   # draw the extra icon
-        draw.text((105, 5), extra_icon, font=ICON_FONT, fill=(255, 255, 255),)
+        d_img_draw.text((105, 5), extra_icon, font=ICON_FONT, fill=(255, 255, 255),)
 
-    disp.image(image)
+    disp.image(d_img)
 
 def rotary_incoming(r):
     #
-    # Triggerd by rotary QT interrupt
+    # Triggered by rotary QT interrupt
     # when rotary wheel is turned or clicked
     #
-    global rotary_pos
+    global rotary_pos, rot_first, rot_last
     current_pos = seesaw.encoder_position()
-    rot_btn = seesaw.digital_read(24)
+    rot_btn = not(seesaw.digital_read(24))
+    print("rotary pos: {0}; current_pos: {1}; rot_btn: {2}".format(rotary_pos, current_pos, rot_btn))
     if rotary_pos == current_pos:
         if rot_btn == True:
             print("Button pressed!")
             button_rotary_click()
     else:
+        #rot_first = datetime.datetime.now()
+        #diff = rot_first - rot_last
         if current_pos < rotary_pos:
             print("Turned right {}".format(current_pos))
             # Action on every other (even) click
@@ -337,6 +379,8 @@ def rotary_incoming(r):
             # Action on every other (even) click
             if (current_pos % 2) == 0:
                 wheel_left()
+        #rot_last = datetime.datetime.now()
+        #print("diff: {}".format(diff.microseconds))
     rotary_pos = current_pos
     #print("seesaw rotary data updated! {0}, {1}".format(rot_pos, rot_btn))
     
@@ -345,20 +389,22 @@ def wheel_right():
     # Wheel turned right (>)
     #
     
-    global noise_pointer, current_volume
+    global disp, noise_pointer, current_volume, image
 
     if display_mode == 0:
         noise_pointer = noise_pointer + 1
         if noise_pointer > len(img_list) - 1:
             noise_pointer = 0
-        image.paste(img_list[noise_pointer], (0,0))  # use now playing image
-        draw.text((1, 110), "\U000F04D7", font=ICON_FONT, fill=(255, 255, 255),)  # draw the right sel icon
+        image.paste(img_list[noise_pointer], (0,0))
+        image.paste(img_right, (1, 110))
+        #draw.text((50, 50), "RIGHT", font=TEXT_FONT, fill=(255, 255, 255),)
+        #draw.text((1, 110), "\U000F04D7", font=ICON_FONT, fill=(255, 255, 255),)  # draw the right sel icon
     elif display_mode == 1:
         current_volume = current_volume + 1
         if current_volume > 19:
             current_volume = 19
         else:
-            image.paste(vol_list[current_volume], (0,0))  # use now playing image
+            image.paste(vol_list[current_volume], (0,0))
     
     disp.image(image)
 
@@ -367,20 +413,21 @@ def wheel_left():
     # Wheel turned left (<)
     #
  
-    global noise_pointer, current_volume
+    global noise_pointer, current_volume, draw
 
     if display_mode == 0:
         noise_pointer = noise_pointer - 1
         if noise_pointer < 0:
             noise_pointer = len(img_list) - 1
-        image.paste(img_list[noise_pointer], (0,0))  # use now playing image
+        image.paste(img_list[noise_pointer], (0,0))
+        image.paste(img_left, (1, 110))
         draw.text((1, 110), "\U000F04D5", font=ICON_FONT, fill=(255, 255, 255),)  # draw the left sel icon
     elif display_mode == 1:
         current_volume = current_volume - 1
         if current_volume < 0:
             current_volume = 0
         else:
-            image.paste(vol_list[current_volume], (0,0))  # use now playing image
+            image.paste(vol_list[current_volume], (0,0))
     
     disp.image(image)
 
@@ -388,30 +435,32 @@ def wheel_left():
 #     %%%%%%%%%%%%  Initial program flow continues here %%%%%%
 #
 
+print("Starting...")
+
 # Set button inputs
 # stop
-GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(5, GPIO.RISING, callback=button_stop, bouncetime=DEBOUNCE)
+GPIO.setup(STOP_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(STOP_GPIO, GPIO.RISING, callback=button_stop, bouncetime=DEBOUNCE)
 
 #display
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(21, GPIO.RISING, callback=button_display, bouncetime=DEBOUNCE+200)
+GPIO.setup(DISP_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(DISP_GPIO, GPIO.RISING, callback=button_display, bouncetime=DEBOUNCE+200)
 
 # preset 1
-GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(6, GPIO.RISING, callback=button_preset, bouncetime=DEBOUNCE)
+GPIO.setup(P1_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(P1_GPIO, GPIO.RISING, callback=button_preset, bouncetime=DEBOUNCE)
 
 # preset 2
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(13, GPIO.RISING, callback=button_preset, bouncetime=DEBOUNCE)
+GPIO.setup(P2_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(P2_GPIO, GPIO.RISING, callback=button_preset, bouncetime=DEBOUNCE)
 
 # preset 3
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(19, GPIO.RISING, callback=button_preset, bouncetime=DEBOUNCE)
+GPIO.setup(P3_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(P3_GPIO, GPIO.RISING, callback=button_preset, bouncetime=DEBOUNCE)
 
 #preset 4
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(26, GPIO.RISING, callback=button_preset, bouncetime=DEBOUNCE)
+GPIO.setup(P4_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(P4_GPIO, GPIO.RISING, callback=button_preset, bouncetime=DEBOUNCE)
 
 #rotaryio interrupt
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -420,12 +469,22 @@ GPIO.add_event_detect(17, GPIO.FALLING, callback=rotary_incoming)
 # set initial display states
 init_controller()
 
+# get saved volume, if available
+redis_conn = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
+v = redis_conn.get("v")
+if v is not(None):
+    current_volume = int(v)
+new_volume = current_volume
+
 while True:
     loop_count = loop_count + 1
     if loop_count == 5:
         loop_count = 0
         #  Check to see if some other process has changed the status
-        r = requests.get('http://noise:5000/')
+        try:
+            r = requests.get('http://noise:5000/')
+        except:
+            print("Error accessing noise service.")
         j = r.json()
         if j["status"] == "stop":
             if now_playing != "":
@@ -435,6 +494,13 @@ while True:
             if now_playing != j["file"]:
                 now_playing = j["file"]
                 display_now(img_list[wav_list.index(now_playing)])
-    
+        # See if we need to save a new volume level in Redis
+        if current_volume != new_volume:
+            # Save volume in Redis
+            try:
+                redis_conn.set('v', current_volume)
+            except:
+                print("Error saving volume data to Redis.")
+            new_volume = current_volume
+
     time.sleep(2)
-    
